ac3aa969c04960f206edbee24e3414812662f6a2
AtennopVer <anton7185569@gmail.com>
2023-03-27 23:32:46 +0300

    Better field displaying

--- GameOfLife/Field/GameFieldView.cs
+++ GameOfLife/Field/GameFieldView.cs
@@ -7,17 +7,16 @@
 
         public void Display(ICell[,] cells)
         {
-            Console.Clear();
-
             var length = cells.GetLength(0);
             var width = cells.GetLength(1);
 
             for (var i = 0; i < length; i++)
             {
                 for (var j = 0; j < width; j++)
+                {
+                    Console.SetCursorPosition(j, i);
                     Console.Write(cells[i, j].IsAlive ? "# " : ". ");
-
-                Console.WriteLine();
+                }
             }
         }
     }
